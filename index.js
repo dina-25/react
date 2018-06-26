c3aee1ce4bd2fdab8508ed6eeba49f17c0f7ab37
Dina <dinaelsayed793@gmail.com>
2018-06-26 16:10:06 +0200

    my correction message

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();                //listen to requests and route them to han
 
 
 //Create a route handler and associate it with a given route
+// '/' indicates root route
 app.get( '/' , (req, res) => {
 	
 	res.send({ hi: 'there' });
 	
 });
 
-app.listen(5000);
+//to dynamically figure out what port should we be listenning to
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
+
+//https://git.heroku.com/dry-waters-25499.git commit target
